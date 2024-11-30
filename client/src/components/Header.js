fee3ea5af6b8980ce2616d183705fdb96b03e96d
Vladimir Holak <vladimirholak@gmail.com>
2024-11-30 12:12:33 +0100

    Added Home page and updated header component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,18 +1,18 @@
 import React from "react";
 import { Navbar, TextInput } from "flowbite-react";
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import UserAvatar from "./UserAvatar";
 import { useSelector, useDispatch } from "react-redux";
 import { setOpenSidebar } from "../redux/slices/authSlice";
 
-export default function Header() {
-  const path = useLocation().pathname;
+export default function Header({ isProtected }) {
   const { user } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
 
   return (
     <Navbar className="border-b-8 border-t-2 border-teal-600 h-30">
+      {/* Logo */}
       <Link
         to="/"
         className="self-center whitespace-nowrap text-sm sm:text-xl font-medium dark:text-white"
@@ -22,28 +22,31 @@ export default function Header() {
         </span>
         <span className="text-gray-500 px-1">App</span>
       </Link>
-      <form>
-        <TextInput
-          type="text"
-          placeholder="Search..."
-          icon={MagnifyingGlassIcon}
-          className="flex-1 outline-none bg-transparent placeholder:text-gray-500 text-gray-800"
-        />
-      </form>
 
-      <div className="flex gap-3 md:order-2">
-        {user && (
-          <>
-            <button
-              onClick={() => dispatch(setOpenSidebar(true))}
-              className="text-2xl text-gray-500 block md:hidden"
-            >
-              ☰
-            </button>
-            <UserAvatar fullName={user.name} />{" "}
-          </>
-        )}
-      </div>
+      {/* Search bar (only for protected routes) */}
+      {isProtected && (
+        <form>
+          <TextInput
+            type="text"
+            placeholder="Search..."
+            icon={MagnifyingGlassIcon}
+            className="flex-1 outline-none bg-transparent placeholder:text-gray-500 text-gray-800"
+          />
+        </form>
+      )}
+
+      {/* User avatar and sidebar toggle (only for protected routes) */}
+      {isProtected && user && (
+        <div className="flex gap-3 md:order-2">
+          <button
+            onClick={() => dispatch(setOpenSidebar(true))}
+            className="text-2xl text-gray-500 block md:hidden"
+          >
+            ☰
+          </button>
+          <UserAvatar fullName={user.name} />
+        </div>
+      )}
     </Navbar>
   );
 }
